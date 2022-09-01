6c1cd197519ddc890e3c2accd8904f6991dad178
Dahyna-Martinez <dahyna.martinez@upr.edu>
2022-09-01 17:50:19 -0400

    Made a file and place a simplie addition program to practice push and pull

---
+++ example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cctype>
+
+using namespace std;
+
+int main(){
+ int num1=8 , num2=7;
+ int sum;
+ sum = num1 + num2;
+cout << sum << endl;
+cout << "This is a practice run" << endl;
+    return 0;
+}
