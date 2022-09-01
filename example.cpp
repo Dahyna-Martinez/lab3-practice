515b1c0a353abb33970db5cd15289bc5ba322a50
Dahyna G. Martínez Pérez <79370563+Dahyna-Martinez@users.noreply.github.com>
2022-09-01 17:56:33 -0400

    Testing the push and pull
    Testing with a simple code and added num3,num4,sub

--- example.cpp
+++ example.cpp
@@ -6,10 +6,13 @@
 using namespace std;
 
 int main(){
- int num1=8 , num2=7;
- int sum;
+ int num1=8 , num2=7, num3 =6, num4= 78;
+ int sum, sub;
  sum = num1 + num2;
+ sub = num3 - num4;
 cout << sum << endl;
 cout << "This is a practice run" << endl;
+cout << "returning to vscode"<< endl;// this is new for vs 
+cout << sub << endl;// this is also new as well as num3 and num4
     return 0;
 }
